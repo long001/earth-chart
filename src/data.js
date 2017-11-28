afef59eda86d80ea93c72bb38c11d91a5de69c19
solocode <solocode@163.com>
2017-11-28 11:27:14 +0800

    upload

--- src/data.js
+++ src/data.js
@@ -1,51 +1,57 @@
-const type_list = [ "正常", "疑似欺诈", "欺诈" ]
-const min = 10, max = 30, group_count = 10, scatter_count = 100, scatter_density = 30
-const density = .8, scale = 1.3
-let node_list = [], links = []
+const type_list = ["正常", "疑似欺诈", "欺诈"]
+const min = 10,
+    max = 30,
+    group_count = 10,
+    scatter_count = 100,
+    scatter_density = 30
+const density = .8,
+    scale = 1.3
+let node_list = [],
+    links = []
 let count = 0;
 
 function getRandomNum(min, max) {
-	const range = max - min;
-	return (min + Math.round(Math.random() * range));
+    const range = max - min;
+    return (min + Math.round(Math.random() * range));
 }
 
 function generateLinks(list, den) {
-	let results = []
-	for(let y=0;y<den;y++){
-		const start = getRandomNum(0, list.length-1)
-		const end = getRandomNum(0, list.length-1)
-		links.push({
-			source: list[start].id,
-			target: list[end].id,
-			value: Math.random() * scale
-		})
-	}
-	return results
+    let results = []
+    for (let y = 0; y < den; y++) {
+        const start = getRandomNum(0, list.length - 1)
+        const end = getRandomNum(0, list.length - 1)
+        links.push({
+            source: list[start].id,
+            target: list[end].id,
+            value: Math.random() * scale
+        })
+    }
+    return results
 }
 
 for (let i = 0; i <= group_count; i++) {
-	const range = getRandomNum(min, max)
-	const type = type_list[ getRandomNum(0, 2) ]
-	let list = []
-	for (let j = 0; j < range; j++) {
-		list.push({
-			group: i,
-			id: count,
-			status: type
-		})
-		count++
-	}
-	//generate links
-	links = links.concat(generateLinks(list, list.length / density))
-	node_list = node_list.concat(list)
+    const range = getRandomNum(min, max)
+    const type = type_list[getRandomNum(0, 2)]
+    let list = []
+    for (let j = 0; j < range; j++) {
+        list.push({
+            group: i,
+            id: count,
+            status: type
+        })
+        count++
+    }
+    //generate links
+    links = links.concat(generateLinks(list, list.length / density))
+    node_list = node_list.concat(list)
 }
 
 let scattered = []
 for (let x = 0; x < scatter_count; x++) {
-	scattered.push({
-		id: count
-	})
-	count++
+    scattered.push({
+        id: count
+    })
+    count++
 }
 
 links = links.concat(generateLinks(scattered, scatter_density))
@@ -53,5 +59,6 @@ links = links.concat(generateLinks(scattered, scatter_density))
 node_list = node_list.concat(scattered)
 
 const data = {
-	nodes: node_list, links: links
+    nodes: node_list,
+    links: links
 }
